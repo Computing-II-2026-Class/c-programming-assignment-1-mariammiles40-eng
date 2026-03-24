72f268612da914c963d52008b813400444632b5a
mariammiles40-eng <mariammiles40@gmail.com>
2026-03-24 20:21:38 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+// Name: NAKANWAGI MARIAM //
+// 2500801403: 25/U/BIE/01403/PE //
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+
+float units_consumed;
+float cost_per_unit;
+float total_bills;
+
+printf("Enter units consumed");
+scanf("%f", &units_consumed);
+
+printf("Enter cost per unit");
+scanf("%f", &cost_per_unit);
+
+total_bill = (units_consumed * cost_per_unit);
+printf("TOTAL BILL:%.2f", total_bill);
+
 
     return 0;
 }
