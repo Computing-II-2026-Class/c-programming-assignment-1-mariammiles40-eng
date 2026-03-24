736ac7a1080df923cd5411a55f6bac0c03227c64
mariammiles40-eng <mariammiles40@gmail.com>
2026-03-24 20:09:10 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,25 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+//Name: NAKANWAGI MARIAM//
+//2500801403: 25/U/BIE/01403/PE //
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+
+float distance_travelled;
+float fuel_used;
+float fuel_efficiency;
+
+printf("Enter distance travelled (km): ");
+scanf("%f", &distance_travelled);
+
+printf("Enter fuel used (litres): ");
+scanf("%f", %fuel_used);
+
+fuel efficiency = (distance_travelled/fuel_used);
+
+printf("FUEL EFFICIENCY:%.2fkm/l", fuel_efficiency);
+    
 
     return 0;
 }
